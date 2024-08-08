043df8d2d60873cc42916d36afc6b070ee7bc6a9
Meyon Soo(Harvy)  Kim <39788337+PreAgile@users.noreply.github.com>
2024-08-08 13:33:26 +0900

    logback 국가 설정 로그 추가

--- src/main/java/kr/flap/market_worker/MarketWorkerApplication.java
+++ src/main/java/kr/flap/market_worker/MarketWorkerApplication.java
@@ -1,12 +1,14 @@
 package kr.flap.market_worker;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class MarketWorkerApplication {
@@ -14,6 +16,7 @@ public class MarketWorkerApplication {
 	@PostConstruct
 	public void started() {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		log.info("Current JVM timezone: " + java.util.TimeZone.getDefault().getID());
 	}
 
 	public static void main(String[] args) {
